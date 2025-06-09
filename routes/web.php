af110cfbdd50accb602d650dbf5a178427b83a1f
Thiago Barbosa Soares <thiagobarso@gmail.com>
2025-06-09 00:00:13 -0300

    138. Conectando ao MySQL de forma dinâmica

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -43,5 +44,34 @@ Route::get('/mysql_test_two_databases', function () {
         echo "<br>";
     } catch (\Exception $e) {
         die("Erro: " . $e->getMessage());
+    }
+});
+
+Route::get('/mysql_test_dynamic_connection', function () {
+    try {
+        Config::set('database.connections.mysql', [
+            'driver' => 'mysql',
+            'url' => '',
+            'host' => 'localhost',
+            'port' => '3306',
+            'database' => 'laravel_mysql_auth',
+            'username' => 'user_laravel_mysql_database',
+            'password' => 'q2WO6aBI637EyoMiLeBi4AqOB2f8yi',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ]);
+
+        DB::connection('mysql')->getPdo();
+        echo 'Ligação OK!';
+    } catch (\Exception $e) {
+        echo 'Deu erro!';
     }
 });
