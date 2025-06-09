b68d67f0252fd7efca8d51ed8edbc8fca3d7d43c
Thiago Barbosa Soares <thiagobarso@gmail.com>
2025-06-08 23:34:38 -0300

    135. Criando e conectando a uma base de dados SQLite

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,16 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/mysql', function () {
+    try {
+        DB::connection()->getPdo();
+        echo "Conexão com a base de dados: " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        die("Não foi possível conectar com a base de dados . Erro: " . $e->getMessage());
+    }
+});
+
+Route::get('/sqlite', function () {
     try {
         DB::connection()->getPdo();
         echo "Conexão com a base de dados: " . DB::connection()->getDatabaseName();
