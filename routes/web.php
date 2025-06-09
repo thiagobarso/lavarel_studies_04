b8b4399aee0c29f1926b3485ab17df9f61e0f63e
Thiago Barbosa Soares <thiagobarso@gmail.com>
2025-06-08 23:26:41 -0300

    134. Criando e conectando a uma base de dados MySQL

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+Route::get('/', function () {
+    try {
+        DB::connection()->getPdo();
+        echo "Conexão com a base de dados: " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        die("Não foi possível conectar com a base de dados . Erro: " . $e->getMessage());
+    }
+});
